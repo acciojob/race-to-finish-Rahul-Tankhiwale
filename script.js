19846cfc396dfc0498482703961f655b44012666
Rahul-Tankhiwale <153199890+Rahul-Tankhiwale@users.noreply.github.com>
2025-11-10 09:46:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+
+let p1 = Promise.resolve("P1", 1000);
+let p2 = Promise.resolve("P2", 2000);
+let p3 = Promise.resolve("P3", 3000);
+let p4 = Promise.resolve("P4", 4000);
+let p5 = Promise.resolve("P5", 5000);
+
+Promise.any([p1,p2,p3,p4,p5])
+.then((res)=>{
+	console.log(result);
+})
+.catch((err)=>{
+	console.log(err);
+})
